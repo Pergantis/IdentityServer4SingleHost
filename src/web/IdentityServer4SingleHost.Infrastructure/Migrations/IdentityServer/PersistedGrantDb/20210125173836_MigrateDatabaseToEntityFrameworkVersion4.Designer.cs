31c92281addbe586f280618dba01f2c249ed723d
ΔΗΜΗΤΡΙΟΣ ΠΕΡΓΑΝΤΗΣ <ceid5056@upatras.gr>
2021-01-25 19:41:18 +0200

    Update Database Schema IdentityServer.EntityFramework 4.1.1

--- src/web/IdentityServer4SingleHost.Infrastructure/Migrations/IdentityServer/PersistedGrantDb/20210125173836_MigrateDatabaseToEntityFrameworkVersion4.Designer.cs
+++ src/web/IdentityServer4SingleHost.Infrastructure/Migrations/IdentityServer/PersistedGrantDb/20210125173836_MigrateDatabaseToEntityFrameworkVersion4.Designer.cs
@@ -4,14 +4,16 @@ using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityServer4SingleHost.Infrastructure.Migrations.IdentityServer.PersistedGrantDb
 {
     [DbContext(typeof(PersistedGrantDbContext))]
-    partial class PersistedGrantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210125173836_MigrateDatabaseToEntityFrameworkVersion4")]
+    partial class MigrateDatabaseToEntityFrameworkVersion4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
